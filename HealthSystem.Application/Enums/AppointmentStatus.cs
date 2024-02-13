2deae80e606085b24ff41060b8a72e4c1b68667c
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-02-13 20:42:46 -0300

    fix status options

--- HealthSystem.Application/Enums/AppointmentStatus.cs
+++ HealthSystem.Application/Enums/AppointmentStatus.cs
@@ -5,10 +5,12 @@ namespace HealthSystem.Application.DTOs.Enums;
 #nullable disable
 public enum AppointmentStatus
 {
-    [Description("Agendado")]
-    Scheduled = 0,
+    [Description("Aguardando confirmação de presença")]
+    awaitConfirmParticipation = 0,
+    [Description("Presença confirmada")]
+    confirmParticipation = 1,
     [Description("Cancelada")]
-    Cancelled = 1,
+    Cancelled = 2,
     [Description("Concluída")]
-    Completed = 2,
+    Completed = 3
 }
